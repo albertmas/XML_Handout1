54e4ffa40c20d75c5a731ad247cde47a24e41db3
albertmas <almaco_io@hotmail.es>
2017-10-10 10:29:02 +0200

    c

--- Dev_class5_handout/Dev_class5_handout/Motor2D/j1Scene.cpp
+++ Dev_class5_handout/Dev_class5_handout/Motor2D/j1Scene.cpp
@@ -63,13 +63,17 @@ bool j1Scene::Update(float dt)
 
 	//App->render->Blit(img, 0, 0);
 	App->map->Draw();
-
+	int mouseX = 0;
+	int mouseY = 0;
+	App->input->GetMousePosition(mouseX, mouseY);
+	
 	// TODO 7: Set the window title like
-	// "Map:%dx%d Tiles:%dx%d Tilesets:%d"
-	p2SString title("Map:%dx%d Tiles:%dx%d Tilesets:%d",
+	// "Map:%dx%d Tiles:%dx%d Tilesets:%d MouseX:%d MouseY:%d"
+	p2SString title("Map:%dx%d Tiles:%dx%d Tilesets:%d MouseX:%d MouseY:%d",
 					App->map->data.width, App->map->data.height,
 					App->map->data.tile_width, App->map->data.tile_height,
-					App->map->data.tilesets.count());
+					App->map->data.tilesets.count(),
+					mouseX, mouseY);
 
 	App->win->SetTitle(title.GetString());
 	return true;
