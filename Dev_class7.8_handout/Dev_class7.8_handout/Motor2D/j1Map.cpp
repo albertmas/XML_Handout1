cb66712a31fcac229494eefff4830efb3922017a
albertmas <almaco_io@hotmail.es>
2017-10-24 10:28:44 +0200

    b

--- Dev_class7.8_handout/Dev_class7.8_handout/Motor2D/j1Map.cpp
+++ Dev_class7.8_handout/Dev_class7.8_handout/Motor2D/j1Map.cpp
@@ -103,6 +103,45 @@ void j1Map::PropagateDijkstra()
 	}
 }
 
+void j1Map::PropagateAStar()
+{
+	// TODO 3: Taking BFS as a reference, implement the Dijkstra algorithm
+	// use the 2 dimensional array "cost_so_far" to track the accumulated costs
+	// on each cell (is already reset to 0 automatically)
+
+	iPoint curr;
+	uint new_cost;
+	if (frontier.Pop(curr))
+	{
+		iPoint neighbors[4];
+		neighbors[0].create(curr.x + 1, curr.y + 0);
+		neighbors[1].create(curr.x + 0, curr.y + 1);
+		neighbors[2].create(curr.x - 1, curr.y + 0);
+		neighbors[3].create(curr.x + 0, curr.y - 1);
+
+		for (uint i = 0; i < 4; ++i)
+		{
+			if (MovementCost(neighbors[i].x, neighbors[i].y) != -1)
+			{
+				new_cost = cost_so_far[curr.x][curr.y] + MovementCost(neighbors[i].x, neighbors[i].y);
+
+				if (cost_so_far[neighbors[i].x][neighbors[i].y] == 0 || new_cost < cost_so_far[neighbors[i].x][neighbors[i].y])
+				{
+					if (visited.find(neighbors[i]) == -1)
+					{
+						frontier.Push(neighbors[i], new_cost);
+						visited.add(neighbors[i]);
+
+						breadcrumbs.add(curr);
+
+						cost_so_far[neighbors[i].x][neighbors[i].y] = new_cost;
+					}
+				}
+			}
+		}
+	}
+}
+
 int j1Map::MovementCost(int x, int y) const
 {
 	int ret = -1;
